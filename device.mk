66e76ac6a9572fea6d0a576e86aa06c5f00d148d
pjgowtham <pjgowtham@gmail.com>
2024-11-01 16:42:48 +0530

    benz: Build oplus IR AIDL
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -21,6 +21,14 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946743308105858.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946743308105858.xml
 
+# IR
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.consumerir.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.ir-service.oplus \
+    consumerir.default
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
