c7bb632b5e3b88087f823efc1e9c0c876d45848a
inferno0230 <mail@inferno0230.in>
2025-09-25 13:31:31 +0530

    benz: Initial PixelOS bringup
    Change-Id: I028208cae5cb16d4fe61ab11a7eaace24ac8d253
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- custom_benz.mk
+++ custom_benz.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from benz device
 $(call inherit-product, device/oneplus/benz/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_benz
 PRODUCT_DEVICE := benz
